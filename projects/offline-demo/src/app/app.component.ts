75416cc0023e7d39fa45d48d16024e031062e976
anass <azeroual.anass2015@gmail.com>
2021-10-19 21:47:41 +0200

    up

--- projects/offline-demo/src/app/app.component.ts
+++ projects/offline-demo/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'offlineDemo';
+  status:boolean = true
+  checkNetworkStatus(e:boolean)
+  {
+    this.status = e;
+  }
 }
